0ae337b7e8d80d89a049450ec21c5f576709e59b
Blog Alpha <sadics@live.com>
2017-01-14 02:58:13 +0000

    Fix creat, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,24 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
     
-    redirect_to articles_show(@article)
+    @article = Article.new(article_params)
+    
+    if @article.save
+      #render plain: params[:article].inspect
+      #flash[:notice] to create an informational message for the user 
+      #it will be displayed on every page using the html wrapper in views/layouts/application.html.erb
+      flash[:notice] = "Article was succesfully created."
+      redirect_to article_path(@article)
+    else
+      #renders a new article.new page
+      render 'new'
+    end
+    
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
