36b352a2d08d78bdcc3d770889b2b18e5e8798a1
Blog Alpha <sadics@live.com>
2017-02-18 22:54:57 +0000

    add admin req to create categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: {case_sensitive: false }, 
-            length: { in: 5..25 }
+            length: { in: 4..25 }
             
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
             
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,6 +5,7 @@ class CategoriesControllerTest < ActionController::TestCase
   #use create here because that way ruby creates and saves in the test db
   def setup
     @category = Category.create(name: "pellegrino")
+    @user = User.create(username: "Batz", email: "batz@gotham.com", password: "password", admin: true)
   end
   
   test "should get categories index" do
@@ -13,6 +14,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -22,6 +24,13 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports"} 
+    end
+    assert_redirected_to categories_path
   end
   
 end
