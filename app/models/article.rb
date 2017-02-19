d595a16f08ad97c86c6775924ac471ce53c4e2df
Blog Alpha <sadics@live.com>
2017-02-19 18:20:51 +0000

    added article_categories table and assoc

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ActiveRecord::Base
+  
+  #setting up the one to many relation of articles table with user table
   belongs_to :user
+  #setting up the many to many relation of articles table with article_categories table
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   validates :title, presence: true, length: { minimum: 5, maximum: 25 }
   validates :description, presence: true, length: { minimum: 15, maximum: 300 }
   validates :user_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ActiveRecord::Base
   
+  #setting up the many to many relation of category table with article_categories table
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
   validates :name, presence: true, length: { minimum: 4, maximum: 25  }
   validates_uniqueness_of :name
   
